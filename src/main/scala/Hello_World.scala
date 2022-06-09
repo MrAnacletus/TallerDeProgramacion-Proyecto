b01f4df43a65eebbfa981ad9bc0cdb2d343f9c1a
Sebastian <sebastian.sepulvedab@sansano.usm.cl>
2022-06-09 17:40:13 -0400

    Configuración inicial, ahora funciona

--- src/main/scala/Hello_World.scala
+++ src/main/scala/Hello_World.scala
@@ -1,3 +1,11 @@
-object Hello_World {
-	println("hello world");
+object Main{
+	def main(args: Array[String]): Unit = {
+		// Me lee un archivo :O
+		val bufferedSource = io.Source.fromFile("src/dataset.csv")
+		for (line <- bufferedSource.getLines) {
+			val linea = line.split(",").map(_.trim)				// Por cada linea se leería una medición en scala, con un delay digamos
+			println(linea(0) + " " + linea(1) + " " + linea(2))	// de un segundo se podría simular un sensor de movimiento
+			Thread.sleep(1000)									// y que se envie un mensaje al brocker
+		}
+	}
 }
